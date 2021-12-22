3da91a3ecb6e6ec2ce93e10d2fc39cc125d1446b
kleitonOak <kleiton.contato@gmail.com>
2021-12-22 15:23:38 -0300

    New Logic

--- src/OneWayString.java
+++ src/OneWayString.java
@@ -1,8 +1,9 @@
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /**
- * There are three types of edits that can be perfomed on string: Insert a character, Remove a character, or Replace a character.
+ * There are three types of edits that can be performed on string: Insert a character, Remove a character, or Replace a character.
  * Given two strings, write a function to check if they are one edit (Or zero edits) away.
  * Example:
  * pale,  ple  -> true
@@ -12,8 +13,8 @@ import java.util.Set;
  */
 public class OneWayString {
     public static void main(String[] args) {
-        String[] firsts = new String[]{"pale", "pales", "pale", "pale"};
-        String[] seconds = new String[]{"ple", "pale", "bale", "bake"};
+        String[] firsts = new String[]{"pale", "pales", "pale", "pale", "heap"};
+        String[] seconds = new String[]{"ple", "pale", "bale", "bake", "eap"};
 
         for(int i=0; i<firsts.length; i++){
             System.out.printf("%s and %s is result = %s \n", firsts[i], seconds[i], isOneWayEdit(firsts[i], seconds[i]));
@@ -21,7 +22,52 @@ public class OneWayString {
     }
 
     public static Boolean isOneWayEdit(String first, String second) {
-        return isOneWayEditUsingSets(first, second);
+        //return isOneWayEditUsingSets(first, second);
+        return  isOneWayEditUsingStringBuffer(first, second);
+    }
+
+    public static Boolean isOneWayEditUsingStringBuffer(String first, String second) {
+        if ((first.length() - second.length()) > 1) {
+            return Boolean.FALSE;
+        }
+
+        Integer diff =  0;
+        Boolean isOneEdit = Boolean.FALSE;
+        int max = first.length()>second.length()?first.length():second.length();
+
+        for(int i = 0; i<max; i++){
+            if(checkString(first, second, i)){
+                isOneEdit = Boolean.TRUE;
+                break;
+            }
+        }
+
+        return isOneEdit;
+
+    }
+
+    private static boolean checkString(String first, String second, int i) {
+        boolean hasSameSize = first.length() == second.length();
+        //REPLACE
+        if(hasSameSize) {
+            StringBuilder tempA = new StringBuilder(first);
+            StringBuilder tempB = new StringBuilder(second);
+            tempA.deleteCharAt(i);
+            tempB.deleteCharAt(i);
+            return Objects.equals(tempA.toString(), tempB.toString());
+        }
+
+        //INSERT OU REMOVE
+        if(first.length() > second.length()){
+            StringBuilder tempA = new StringBuilder(first);
+            tempA.deleteCharAt(i);
+            return Objects.equals(tempA.toString(), second);
+        }else{
+            StringBuilder tempB = new StringBuilder(second);
+            tempB.deleteCharAt(i);
+            return Objects.equals(tempB.toString(), first);
+        }
+
     }
 
     public static Boolean isOneWayEditUsingSets(String first, String second) {
