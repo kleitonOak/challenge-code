70e8734714f416d190930dc88051dfb433a31c71
kleitonOak <kleiton.contato@gmail.com>
2021-12-22 17:04:13 -0300

    Rotate Matrix 90 skeleton

---
+++ src/RotateMatrix90Degrees.java
@@ -0,0 +1,26 @@
+public class RotateMatrix90Degrees {
+    public static void main(String[] args)
+    {
+        //4X5
+        Integer[][] matrix = new Integer[][]{{1,2,3,4,10},{5,6,7,8,10},{9,10,11,12,10},{13,14,15,16,10}};
+
+        System.out.printf("Matrix %dx%d\n",matrix.length, matrix[0].length);
+        printMatrix(matrix);
+        rotateMatrix(matrix);
+        printMatrix(matrix);
+    }
+
+    private static Integer[][] rotateMatrix(Integer[][] matrix){
+       //TODO :-(
+        return matrix;
+    }
+
+    private static void printMatrix(Integer[][] matrix){
+        for(int r=0; r<matrix.length; r++){
+            for(int c=0; c<matrix[r].length; c++){
+                System.out.printf("Value at %dx%d = %d \t", r,c,matrix[r][c]);
+            }
+            System.out.printf("\n");
+        }
+    }
+}
